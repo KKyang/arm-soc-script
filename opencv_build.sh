a42d6b9b8b496d712dd619a1d729370c10acd958
tonyhccdev <tonyhccdev@gmail.com>
2015-02-09 21:27:08 +0800

    add log

--- opencv_build.sh
+++ opencv_build.sh
@@ -22,6 +22,6 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_DOCS=OFF -DBUILD_EXAMPLES=OFF \
  -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_java=OFF \
  -DENABLE_NEON=ON -DWITH_QT=OFF -DWITH_TBB=ON -DWITH_OPENGL=ON \
- -DPYTHON_EXECUTABLE=/usr/bin/python2.7 ../
-make 
+ -DPYTHON_EXECUTABLE=/usr/bin/python2.7 ../ > cmake_log
+make > build.log
 sudo make install

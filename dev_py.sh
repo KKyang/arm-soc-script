3212ea493b540346dc992836c073c48a39039ffa
tonyhccdev <tonyhccdev@gmail.com>
2015-02-09 20:56:07 +0800

    fix package name typo

--- dev_py.sh
+++ dev_py.sh
@@ -2,8 +2,8 @@
 
 # python env
 sudo apt-get -y install python-dev python-numpy python-matplotlib python-serial \
-ipython-notebook python2-pyside python-pyqt4 python-pyqt5 \
-python3-dev python3-numpy python3-matplotlib python3-pyserial ipython3-notebook \
+ipython-notebook python-pyside python-qt4 python-pyqt5 \
+python3-dev python3-numpy python3-matplotlib python3-serial ipython3-notebook \
 python3-pyside python3-pyqt4 python3-pyqt5 \
 pyqt4-dev-tools pyqt5-dev-tools
 # ide

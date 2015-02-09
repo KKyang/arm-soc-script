3200826058e658e581f383420a0fa1db4d188426
tonyhccdev <tonyhccdev@gmail.com>
2015-02-09 19:48:58 +0800

    remove dev_env script add different env for cpp and python

---
+++ dev_cpp.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# build env
+sudo apt-get -y install build-essiential git cmake 
+# ide
+sudo apt-get -y install qtcreator codelite
+
--- dev_env.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-sudo apt-get -y install python3-pyside python3-pyqt4 python3-pyqt5 \
-qtcreator pyqt4-dev-tools pyqt5-dev-tools \
-python3-matplotlib python3-numpy ipython3 eric codeblocks \
-guvcview qv4l2 

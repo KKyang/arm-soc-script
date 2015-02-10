f22c73602e333ea235a757bb32d4fa54bb25b818
tonyhccdev <tonyhccdev@gmail.com>
2015-02-10 22:28:04 +0800

    add vim

--- dev_cpp.sh
+++ dev_cpp.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # build env
-sudo apt-get -y install build-essiential git cmake 
+sudo apt-get -y install build-essiential git cmake vim 
 # ide
 sudo apt-get -y install qtcreator codelite
 
